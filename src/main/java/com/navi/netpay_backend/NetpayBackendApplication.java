e35754d2b17e8be018f9896287113aeb2e71b07d
Dónavin Tzunun <tzunundonavin@gmail.com>
2026-08-23 18:00:28 -0600

    feat: create project

---
+++ src/main/java/com/navi/netpay_backend/NetpayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.navi.netpay_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetpayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetpayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navi/netpay_backend/NetpayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navi.netpay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetpayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
